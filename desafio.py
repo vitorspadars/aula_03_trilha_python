9bf05857a54942f46baae10b6529f8ad9e103bba
vitorspada <vitorspada.rs@gmail.com>
2026-06-08 21:42:24 -0300

    Conclusão do desafio da aula 3

---
+++ desafio.py
@@ -0,0 +1,42 @@
+nome_valido = False
+salario_valido = False
+bonus_valido = False
+
+while nome_valido is not True:
+    try:
+        nome = input("Digite seu nome: ")
+        # Verifica se o nome está vazio
+        if len(nome.strip()) == 0:
+            raise ValueError("O nome não pode estar vazio.")
+        # Verifica se há números no nome
+        elif any(char.isdigit() for char in nome):
+            raise ValueError("O nome não deve conter números.")
+        else:
+            print("Nome válido:", nome)
+            nome_valido = True
+    except ValueError as e:
+        print(e)
+
+while salario_valido is not True:
+    try:
+        salario = float(input("Digite o valor do seu salário: "))
+        if salario < 0:
+            print("Por favor, digite um valor positivo para o salário.")
+        else:
+            salario_valido = True
+    except ValueError:
+        print("Entrada inválida para o salário. Por favor, digite um número.")
+
+while bonus_valido is not True:
+    try:
+        bonus = float(input("Digite o valor do bônus recebido: "))
+        if bonus < 0:
+            print("Por favor, digite um valor positivo para o bônus.")
+        else:
+            bonus_valido = True
+    except ValueError:
+        print("Entrada inválida para o bônus. Por favor, digite um número.")
+
+bonus_recebido = 1000 + salario * bonus 
+
+print(f"{nome}, seu salário é R${salario:.2f} e seu bônus final é R${bonus_recebido:.2f}.")
